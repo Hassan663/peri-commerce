0ecfc2d998b07e7377db74b5dc53c57b45647c3a
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-22 01:03:05 +0500

    already Login Issue[FIX]

--- src/router/Tab/index.js
+++ src/router/Tab/index.js
@@ -3,13 +3,12 @@ import {
   NavigationContainer,
 } from '@react-navigation/native';
 
-import { createStackNavigator } from '@react-navigation/stack';
 import auth from '@react-native-firebase/auth';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Splash from '../../pages/Splash/Splash';
 import WalkThrough from '../../pages/WalkThrough/WalkThrough';
 import Login from '../../pages/Login/Login';
-import Home from '../../pages/Home/Home';
 import Signup from '../../pages/Signup/Signup';
 import ForgetPass from '../../pages/ForgetPass/ForgetPass';
 import AppTabs from '../BottomTab/BottomTabs';
@@ -38,7 +37,6 @@ function RootNavigator() {
       setUser(user);
     });
 
-    // Clean up the subscription when the component unmounts
     return () => unsubscribe();
   }, []);
 
@@ -46,36 +44,28 @@ function RootNavigator() {
 
 
   return (
-    // <NavigationContainer>
-    // <Stack.Navigator>
-    //   {user ? (
-    //     <Stack.Screen name="AppTabs" component={AppTabs} />
-    //   ) : (
-    //     <Stack.Screen name="Splash" component={Splash} />
-    //   )}
-    // </Stack.Navigator>
-    // {/* </NavigationContainer> */}
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName={user ? 'AppTabs' : 'Splash'}
-    >
-      {/* {user ? (
-          <Stack.Screen name="AppTabs" component={AppTabs} />
-        ) : (
-          <Stack.Screen name="Splash" component={Splash} />
-        )} */}
-      <Stack.Screen name="AppTabs" component={AppTabs} />
-      <Stack.Screen name="Splash" component={Splash} />
-      <Stack.Screen name="WalkThrough" component={WalkThrough} />
-      <Stack.Screen name="Category" component={Category} />
-      <Stack.Screen name="ProductDetail" component={ProductDetail} />
-      <Stack.Screen name="MyCart" component={MyCart} />
-      <Stack.Screen name="CheckOut" component={CheckOut} />
-      <Stack.Screen name="PlaceOrder" component={PlaceOrder} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="ForgetPass" component={ForgetPass} />
-      {/* <Stack.Screen name="Home" component={Home} /> */}
-    </Stack.Navigator>
+    user ? (
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }
+        }
+      >
+        <Stack.Screen name="AppTabs" component={AppTabs} />
+        <Stack.Screen name="ProductDetail" component={ProductDetail} />
+        <Stack.Screen name="MyCart" component={MyCart} />
+        <Stack.Screen name="CheckOut" component={CheckOut} />
+        <Stack.Screen name="PlaceOrder" component={PlaceOrder} />
+        <Stack.Screen name="Category" component={Category} />
+      </Stack.Navigator >
+    ) : (
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="WalkThrough" component={WalkThrough} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="ForgetPass" component={ForgetPass} />
+      </Stack.Navigator>
+    )
   );
 }
