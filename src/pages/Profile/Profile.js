ff8415850e29ac9aaf0878d04c1ecae63f3657f6
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-21 22:44:35 +0500

    profile ui[ADD]

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,17 +1,46 @@
 // @app
-import React from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import {
   View,
-  Image,
   Text,
 } from 'react-native';
 
+import auth from '@react-native-firebase/auth';
 import { styles } from './styles';
+import Button from '../../components/Button';
 
 const Profile = ({ navigation }) => {
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    const currentUser = auth().currentUser;
+    if (currentUser) { setUserProfile(currentUser); }
+  }, []);
+  const handleLogout = async () => {
+    try {
+      await auth().signOut();
+      // Navigate to the login screen or any other appropriate screen
+      navigation.navigate('Login'); // Update 'Login' with the actual screen name
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <View style={styles.titleContainer}>
+        <Text style={[styles.title, styles.line]}>{userProfile?._user?.displayName}</Text>
+        <Text style={styles.title}>{userProfile?._user?.email}</Text>
+      </View>
+      <View style={styles.logOutContainer}>
+
+        <Button
+          callBack={() => handleLogout()}
+          title={'Log Out'}
+          primary />
+      </ View>
     </View >
   );
 };
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -5,8 +5,49 @@ import { RFPercentage } from 'react-native-responsive-fontsize';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.primary,
+        backgroundColor: Colors.white,
+        // justifyContent: 'center',
+        // alignItems: "center"
+    },
+    circle: (size) => ({
+        height: size,
         justifyContent: 'center',
-        alignItems: "center"
+        alignItems: 'center',
+        width: size,
+        backgroundColor: Colors.lightGray,
+        borderRadius: size / 2
+    }),
+    logOutContainer: {
+        height: 50,
+        width: "80%",
+        position: 'absolute',
+        zIndex: 2,
+        bottom: '15%',
+        alignSelf: "center"
+    },
+    line: { textDecorationLine: 'underline', },
+    title: {
+        fontSize: 16,
+        width: "90%",
+        marginVertical: RFPercentage(.5),
+        alignSelf: "center",
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    titleContainer: {
+        marginVertical: RFPercentage(4.5),
+        paddingVertical: RFPercentage(2),
+        backgroundColor: Colors.white,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+        elevation: 7,
+        borderRadius: RFPercentage(1.5),
+        width: "90%",
+        alignSelf: "center"
     },
 });
