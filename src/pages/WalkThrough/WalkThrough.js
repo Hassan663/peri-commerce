1a136bc0a58f726a6b5e34ce5525d8e5e734235a
muzammil hussain shah <mynameismuzammilhussainshah@gmail.com>
2023-08-24 01:06:11 +0500

    interstitial [ADD]

--- src/pages/WalkThrough/WalkThrough.js
+++ src/pages/WalkThrough/WalkThrough.js
@@ -1,5 +1,5 @@
 // @app
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Image,
   SafeAreaView,
@@ -10,9 +10,32 @@ import { useRef } from 'react';
 
 import { styles } from './styles';
 import { Slide } from './Components/Slide';
+import { AppOpenAd, InterstitialAd, RewardedAd, BannerAd, TestIds, BannerAdSize, AdEventType, RewardedAdEventType } from 'react-native-google-mobile-ads';
+
+// const interstitialId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-3340464236132360/1792547171';
+const interstitialId = 'ca-app-pub-3340464236132360/1792547171';
+// const adUnitId2 = 'ca-app-pub-3340464236132360/2693413925';
+
+
+const interstitial = InterstitialAd.createForAdRequest(interstitialId, {
+  requestNonPersonalizedAdsOnly: true,
+  keywords: ['fashion', 'clothing'],
+});
+
+
+
 
 const WalkThrough = ({ navigation }) => {
   const swiperRef2 = useRef(null)
+
+   {/* INTERSTITIAL */ }
+   useEffect(async () => {
+    const unsubscribe = interstitial.addAdEventListener(AdEventType.LOADED, () => { interstitial.show(); });
+    interstitial.load();
+    return unsubscribe;
+  }, [])
+  {/* INTERSTITIAL */ }
+
 
   return (
     <SafeAreaView style={styles.container}>
